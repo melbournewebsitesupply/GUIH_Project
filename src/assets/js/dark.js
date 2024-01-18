6d5c7ea26bf6dabc414cf378e4ae103c31387559
melbournewebsitesupply <melbournewebsitesupply@gmail.com>
2024-01-18 23:31:43 +1100

    removed dark js, potentially github isn't being updated properly

--- src/assets/js/dark.js
+++ src/assets/js/dark.js
@@ -1,4 +1,4 @@
-//
+/* //
 //    The Dark Mode System
 //
 
@@ -38,3 +38,4 @@ document.getElementById('dark-mode-toggle').addEventListener('click', () => {
 	// on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
 	localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
 });
+*/
